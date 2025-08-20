5161c3247d7daff5d6058c70de596a53214d868c
Cewll <yannbug9@gmail.com>
2025-08-20 18:11:08 +0000

    update MainActivity.kt

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background)
                 {
-                    GreetingText("Happy Birthday Cesar !", "From Emma")
+                    GreetingText("Happy Birthday Balou !", "From Emma")
                 }
             }
         }
